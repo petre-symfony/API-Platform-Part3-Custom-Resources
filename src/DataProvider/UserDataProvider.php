d0bbdfc52368112363bd15cb26f96d750b0615dd
petrero <petrero@gmail.com>
2020-12-17 12:12:09 +0200

    11.2. Adding & Populating the Custom Field
    Setting Custom Data in the Data Provider
    Let's finally set this field in UserDataProvider. My guess is that the getCollection() method will return an array of users... but let's actually check that. Add $users =, dd() and, at the bottom, return $users.
    Back at the browser, find the original tab and refresh. Oh! It's not an array of users! It's a Paginator object with a Doctrine Paginator inside! So... this obviously isn't an array, but the Paginator object is iterable: we can loop over it like an array and then update the isMe field on each item.
    Above this line, let's add some documentation to help my editor: I'll advertise that $users is an array of User objects... which is actually a lie... but when we loop over it, we will get User objects.

--- src/DataProvider/UserDataProvider.php
+++ src/DataProvider/UserDataProvider.php
@@ -6,7 +6,6 @@ use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\User;
-use App\Repository\UserRepository;
 
 class UserDataProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface {
 	private $collectionDataProvider;
@@ -14,8 +13,14 @@ class UserDataProvider implements ContextAwareCollectionDataProviderInterface, R
 	public function __construct(CollectionDataProviderInterface $collectionDataProvider){
 		$this->collectionDataProvider = $collectionDataProvider;
 	}
+
 	public function getCollection(string $resourceClass, string $operationName = null, array $context = []) {
-		return $this->collectionDataProvider->getCollection($resourceClass, $operationName, $context);
+		/** @var User[] $users */
+		$users = $this->collectionDataProvider->getCollection($resourceClass, $operationName, $context);
+
+		dd($users);
+
+		return $users;
 	}
 
 	public function supports(string $resourceClass, string $operationName = null, array $context = []): bool {
