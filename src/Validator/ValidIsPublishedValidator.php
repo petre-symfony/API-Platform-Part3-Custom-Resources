1ec2c130c0f2ec8cf0051bda93a2e8279b004e9b
petrero <petrero@gmail.com>
2020-12-17 10:30:08 +0200

    8.3. Publish State Change Validator Logic
    Allowing Admin Users to Publish
    It was failing before because when we log in as an admin user, it was also returning a 400 status code instead of allowing this. To fix that, we need to see if the user is an admin. Add a second argument to the constructor Security $security. I'll initialize this property.
    Then below, update the if statement: if the description is too short and not $this->security->isGranted('ROLE_ADMIN'), fail validation. I'll add a comment to summarize this
    This is true test-driven development. Try the test again:
    
      symfony php bin/phpunit --filter=testPublishCheeseListingValidation
    It fails... but we are further!
    
      Normal user cannot unpublish
    
    If we look at the test... yea! The second and third cases are now passing and we're down to the unpublish logic. You can start to see why breaking each test case into its own method might be a bit cleaner, even if it's more work up-front.

--- src/Validator/ValidIsPublishedValidator.php
+++ src/Validator/ValidIsPublishedValidator.php
@@ -4,14 +4,20 @@ namespace App\Validator;
 
 use App\Entity\CheeseListing;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class ValidIsPublishedValidator extends ConstraintValidator {
 	private $entityManager;
+	private $security;
 
-	public function __construct(EntityManagerInterface $entityManager) {
+	public function __construct(
+		EntityManagerInterface $entityManager,
+		Security $security
+	) {
 		$this->entityManager = $entityManager;
+		$this->security = $security;
 	}
 
 	public function validate($value, Constraint $constraint) {
@@ -32,7 +38,11 @@ class ValidIsPublishedValidator extends ConstraintValidator {
 		if ($value->getIsPublished()) {
 		  // we are publishing
 
-			if (strlen($value->getDescription()) < 100) {
+			//don't allow short description unless you are an admin
+			if (
+				strlen($value->getDescription()) < 100 &&
+				!$this->security->isGranted('ROLE_ADMIN')
+			) {
 				$this->context->buildViolation('Cannot publish: description is too short')
 					->atPath('description')
 					->addViolation();
