5207477063260bc74957371cf61e16f86de42d8d
petrero <petrero@gmail.com>
2020-12-24 21:32:53 +0200

    30.3. Filter Autowiring
    Let's go add it! The only weird part in an entity filter is that our parent class already has a constructor. This means we can't just add a new constructor, we need to override the existing one.
    To do that, go to "Code"->"Generate" - or Command + N on a Mac - select "Override Methods" and choose __construct()

--- src/ApiPlatform/CheeseSearchFilter.php
+++ src/ApiPlatform/CheeseSearchFilter.php
@@ -4,8 +4,16 @@ namespace App\ApiPlatform;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 
 class CheeseSearchFilter extends AbstractFilter {
+  public function __construct(ManagerRegistry $managerRegistry, ?RequestStack $requestStack = null, LoggerInterface $logger = null, array $properties = null, NameConverterInterface $nameConverter = null){
+    parent::__construct($managerRegistry, $requestStack, $logger, $properties, $nameConverter);
+  }
+
   protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null){
     if ($property !== 'search'){
       return;
