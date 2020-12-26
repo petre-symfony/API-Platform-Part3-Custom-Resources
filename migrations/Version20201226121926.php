a6825180e7df5d949f439dc0c6f0f91858746d41
petrero <petrero@gmail.com>
2020-12-26 14:25:23 +0200

    Repeat uuid process for User

---
+++ migrations/Version20201226121926.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201226121926 extends AbstractMigration {
+  public function getDescription() : string {
+    return '';
+  }
+
+  public function up(Schema $schema) : void {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE user ADD uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\'');
+    $this->addSql('UPDATE user SET uuid = UUID()');
+    $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649D17F50A6 ON user (uuid)');
+  }
+
+  public function down(Schema $schema) : void {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->addSql('DROP INDEX UNIQ_8D93D649D17F50A6 ON user');
+    $this->addSql('ALTER TABLE user DROP uuid');
+  }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Nonstandard\Uuid;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -47,6 +48,11 @@ class User implements UserInterface {
    * @ORM\Column(type="integer")
    */
   private $id;
+
+  /**
+   * @ORM\Column(type="uuid", unique=true)
+   */
+  private $uuid;
 
   /**
    * @ORM\Column(type="string", length=180, unique=true)
@@ -111,6 +117,7 @@ class User implements UserInterface {
 
   public function __construct() {
     $this->cheeseListings = new ArrayCollection();
+    $this->uuid=Uuid::uuid4();
   }
 
   public function getId(): ?int {
