cae708be1ae97947cf942fbe60b32387050d25e9
petrero <petrero@gmail.com>
2020-12-26 12:54:39 +0200

    45.3.UUID's
    The only tricky thing is that because we do already have a database with users in it - the fact that this column is NOT NULL will make the migration fail because the existing records will have no value.
    To fix this, temporarily change it to DEFAULT NULL.
    Then, right after this, say $this->addSql() with UPDATE cheese_listing SET uuid = UUID()
    That's a MySQL function to generate UUID's.

--- migrations/Version20201226103551.php
+++ migrations/Version20201226103551.php
@@ -17,7 +17,8 @@ final class Version20201226103551 extends AbstractMigration {
 
   public function up(Schema $schema) : void {
     // this up() migration is auto-generated, please modify it to your needs
-    $this->addSql('ALTER TABLE cheese_listing ADD uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\'');
+    $this->addSql('ALTER TABLE cheese_listing ADD uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\'');
+    $this->addSql('UPDATE cheese_listing SET uuid = UUID()');
     $this->addSql('CREATE UNIQUE INDEX UNIQ_356577D4D17F50A6 ON cheese_listing (uuid)');
   }
 
