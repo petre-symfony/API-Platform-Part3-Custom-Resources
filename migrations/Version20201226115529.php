500a2a554933e0eb179fced842a7f1875df34e25
petrero <petrero@gmail.com>
2020-12-26 14:10:38 +0200

    45.4.UUID's
    Let's try this! Back at your terminal, run the migration:
    
      symfony console doctrine:migrations:migrate
    It works! Now generate one more migration as a lazy way to set the field back to NOT NULL:
    
      symfony console make:migration
    If you look at the new migration. Perfect! This changes uuid from DEFAULT NULL to NOT NULL
    Run the migrations one last time:
    
      symfony console doctrine:migrations:migrate

---
+++ migrations/Version20201226115529.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201226115529 extends AbstractMigration {
+  public function getDescription() : string {
+    return '';
+  }
+
+  public function up(Schema $schema) : void {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE cheese_listing CHANGE uuid uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\'');
+  }
+
+  public function down(Schema $schema) : void {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE cheese_listing CHANGE uuid uuid CHAR(36) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:uuid)\'');
+  }
+}
